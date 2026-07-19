bcb9d2493b12d36b825d18876160456061fabd55
IlMelons <melonss2002@gmail.com>
2026-07-19 19:35:58 +0200

    chore(spatial): initialize data architecture

---
+++ api/shared/grid.lua
@@ -0,0 +1,10 @@
+---@note Architectural placeholder (Data Understanding)
+
+local entries = {}
+
+--[[@simulationdata Entries Store
+    Direct access entry functions   -->     entries[id][action]     -->     (function call)
+]]
+
+---@todo Study metadata
+---@todo Study data structure
---
+++ client/spatial.lua
@@ -0,0 +1,11 @@
+---@note Architectural placeholder (Data Understanding)
+
+local cells = {}
+
+--[[@simulationdata Entries Store
+    Direct access entry data pull   -->     cells[id][entryId]  -->     (One entry in cell)
+    Direct access cells data pull   -->     cells[id]           -->     (All entries in cell)
+]]
+
+---@todo Study networking architecture
+---@todo Study access for all cases
